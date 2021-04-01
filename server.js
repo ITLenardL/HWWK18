1c33df1532cbbd8ec7f9aa388d873b0b1191fca7
lenny <llaska@live.com>
2021-04-01 19:19:19 +1100

    deploying

--- server.js
+++ server.js
@@ -15,8 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
@@ -24,3 +31,5 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
